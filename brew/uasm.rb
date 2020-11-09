fa894dc48ba9206e6f38aae86dc508a7adaa4377
Jupiter <Jupiter.rnd@Gmail.com>
2020-11-09 03:17:48 +0300

    Update uasm.rb

--- brew/uasm.rb
+++ brew/uasm.rb
@@ -4,7 +4,7 @@ class Uasm < Formula
   homepage "https://github.com/upiter/uasmac"
   version "2.50"
   url "https://github.com/upiter/uasmac/releases/download/2.50a/uasm250a.zip"
-  sha256 "0e6e05e5dfd3b9b8009701a3fce6119130ce340845f1588a0387770b9cf45ab6"
+  sha256 "3febb7f97bbe60c1a18d8114853968bdad2c3c284d772542a507a2b85d076390"
   # head "https://github.com/Terraspace/UASM.git", branch: "2.50"
 
   def install
